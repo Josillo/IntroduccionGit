36b6f403ae809340f6539123eb3c67e25ff37fe7
joseanj <jose.jaramago101@gmail.com>
2021-06-16 20:50:17 +0200

    clase Docente creada

---
+++ src/docentes/docente.java
@@ -0,0 +1,17 @@
+package docentes;
+
+public class docente {
+    private String nombre;
+
+    public docente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
